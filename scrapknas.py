3a215152d45c9d18e8841787d44fe56cdc5f3e4d
wouter Prommel <wouterprommel@gmail.com>
2020-02-24 16:21:57 +0100

    via commandline argumenten

--- scrapknas.py
+++ scrapknas.py
@@ -1,12 +1,13 @@
 """Scraper om Ranklijsten van Knas af te halen"""
 
+import sys
 import csv
 from bs4 import BeautifulSoup
 import requests
 
 
 def save(to, content):
-    with open(to, mode='w') as csv_file:
+    with open("rank/" + to, mode='w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         sort = 1
@@ -32,25 +33,33 @@ def save(to, content):
             sort += 1
 
 
-print("opgeslagen als Gender_Wapen_Categorie bijv. H_F_S.csv")
-alles = True;
-# q1 = input("Alles (y/n)")
-''' if (q1 == "y"):
-    alles = True
-elif (q1 == "n"):
-    q2 = input("wapen: (F/E/S) ")
-    q3 = input("gender: (M/F)")
-    q4 = input("leeftijd cat (jeugd/senioren/veteranen): ")
-else:
-    print("enter (y/n)")
-    raise SystemExit
-'''
+def check_file(use_file):
+    parts = use_file.split('.')
+    if len(parts) == 2 and parts[1] == "csv":
+        return True
+    else:
+        run_help()
+        return False
 
-with open("categorien.csv", "r") as categorien:
-    for cat in categorien:
-        line = cat.rstrip().split(',')
-        parts = line[0].split('_')
-        if (alles or True):
+
+def run_help():
+    print("HELP")
+    print("Gebruik: python scrapknas.py 'filename.csv'")
+    print("De 'filenames' zijn te vinde in het mapje categorien")
+    print("De ranklijsten worden opgeslagen als Gender_Wapen_Categorie bijv. H_F_S.csv")
+    exit()
+
+
+if len(sys.argv) == 2:
+    use_file = sys.argv[1]
+else:
+    run_help()
+
+if check_file(use_file):
+    with open("categorien/" + use_file, "r") as categorien:
+        for cat in categorien:
+            line = cat.rstrip().split(',')
+            parts = line[0].split('_')
             response = requests.get(line[1], timeout=5)
             content = BeautifulSoup(response.content, "html.parser")
             line[0] += '.csv'
